e42df52b4ed1380e6c6be4a6fa384c1b2ed6b4a9
melqui <melcelmelquisedeque@gmail.com>
2026-04-27 22:23:15 +0100

    Atualizacoes feitas

--- src/components/api/ApiConfig.js
+++
@@ -1,6 +0,0 @@
-//export const API_URL = 'http://127.0.0.1:8000/api';
-//export const API_URL = 'http://192.168.2.102:8000/api';
-
- 
-
-export const API_URL ='https://qrorder.technext.ao/api';
--- src/components/formatarValor.js
+++
@@ -1,9 +0,0 @@
-export default function formatarValor(valor) {
-  if (typeof valor !== 'number') valor = parseFloat(valor);
-  if (isNaN(valor)) return '0,00';
-  
-  return valor
-    .toFixed(2)
-    .replace('.', ',')
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-}
